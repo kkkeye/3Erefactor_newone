34fd4910d98c617ee697a178cafa0c5aaa644d02
云儿在流浪 <1239742887@qq.com>
2023-10-25 22:20:37 +0800

    设定build编译生成的文件名格式

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    //js output config
+    output: {
+      filename: 'js/[name]-js-out.js',
+      chunkFilename: 'js/[name]-chunk-vendors-out.js'
+    },
+		// css output config
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `css/[name]-css-out.css`,
+        // chunkFilename: `css/[name].${conf.version}.css`
+      })
+    ]
+  }
+
 })
