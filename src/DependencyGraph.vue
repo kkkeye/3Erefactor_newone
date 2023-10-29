0ca5dd85e8d3bc632f616de077d98298c5317544
云儿在流浪 <1239742887@qq.com>
2023-10-29 16:27:15 +0800

    目标模型不再显示父子之间的边

--- src/DependencyGraph.vue
+++ src/DependencyGraph.vue
@@ -14,33 +14,52 @@ export default {
   name: 'DependencyGraph',
   data() {
     return {
+      projectName: "depends",
       data: [],
       entityRoot: [],
       parentIdMap: new Map(),
       entityIdMap: new Map(),
       relations: [],
-      configData:{},
+      configData: {},
+      type: "",
+      packageList:[]
     }
   },
   created() {
     // this.getEntity()
-    this.getConfig()
+    // this.getConfig()
+    this.configData={
+      "targetFilePath": "D:\\git\\refactor-service-test\\depends.con.json",
+        "projectRoot": "D:\\git\\refactor-service-test\\test\\depends",
+        "projectName": "depends",
+    }
+    //dependency,target
+    this.type = 'dependency';
+    this.getEntity()
   },
-  mounted(){
+  mounted() {
     window.addEventListener('message', event => {
       const message = event.data; // The json data that the extension sent
       switch (message.command) {
         case 'setConfig':
           {
             console.log(message.configData);
-            this.configData=JSON.parse(message.configData);
+            this.configData = JSON.parse(message.configData);
+            this.type = message.type;
             this.getEntity()
+            // this.getArchModelData()
             break;
           }
       }
     })
   },
   methods: {
+    getEntityCategory(node) {
+      if (node["External"] === true) {
+        return "External";
+      }
+      return node["category"];
+    },
     getRelationCategory(cell) {
       for (const key of Object.keys(cell["values"])) {
         if (key === "loc" || key === "bindVar" || key === "modifyAccessible" || key === "invoke" || key === "arguments") {
@@ -50,7 +69,7 @@ export default {
       }
       return undefined;
     },
-    async getRelationsByIds(entityIds) {
+    getRelationsByIds(entityIds) {
       const relations = []
       for (const cell of this.data["cells"]) {
         if (entityIds.has(cell["src"]) && entityIds.has(cell["dest"])) {
@@ -59,11 +78,70 @@ export default {
       }
       return relations;
     },
-    async getChildrenById(entityId) {
+    //目标模型的cell加一个判断类型，把父子节点的边不显现。
+    getRelationsByIdsTarget(entityIds) {
+      const relations = []
+      for (const cell of this.data["cells"]) {
+        if (entityIds.has(cell["src"]) && entityIds.has(cell["dest"])) {
+          let category='Relation'
+          let cn=this.packageList.find(n=>n.id==cell["dest"]+'');
+          if(cn.parentId==cell["src"]+''){
+            category='Contain'
+          }
+          let c={src:cell["src"],dest:cell["dest"],category:category}
+          relations.push(c);
+        }
+      }
+      return relations;
+    },
+    getChildrenById(entityId) {
       if (!this.parentIdMap.has(entityId)) {
         return [];
       }
       return this.parentIdMap.get(entityId);
+    },
+    async buildVariableList(variables) {
+      const nameIdMap = new Map();
+      const resultList = [];
+      variables.forEach((variable, i) => {
+        nameIdMap.set(variable, i + 1);
+        let parentId;
+        if (!variable.includes("/")) {
+          parentId = -1;
+        } else {
+          parentId = nameIdMap.get(variable.slice(0, variable.lastIndexOf("/")));
+        }
+        resultList.push({
+          "id": i + 1,
+          "name": variable,
+          "parentId": parentId,
+        });
+      });
+      return resultList;
+    },
+    async getArchModelData() {
+      const params = JSON.stringify({
+        "filePath": this.configData.targetFilePath,
+        "rootDir": this.configData.projectRoot,
+        "projectName": this.configData.projectName,
+      });
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      const result = await axios.post('http://localhost:8888/target/upload', params, config);
+      this.data = result.data.data;
+      console.log(this.data);
+      this.packageList = await this.buildVariableList(this.data["variables"]);
+      console.log(this.packageList);
+      this.packageList.forEach((variable, index) => {
+        this.entityIdMap.set(index, variable);
+        if (!this.parentIdMap.has(variable.parentId)) {
+          this.parentIdMap.set(variable.parentId, []);
+        }
+        this.parentIdMap.get(variable.parentId).push(variable);
+      });
     },
     async getData() {
       const params = JSON.stringify({
@@ -71,7 +149,6 @@ export default {
         lang: "java",
         projectName: this.configData.projectName,
       })
-      console.log(params);
       const config = {
         headers: {
           'Content-Type': 'application/json'
@@ -89,22 +166,33 @@ export default {
       vscode.postMessage({//向vscode插件通信
         command: "hello",
         text: "Dependendcy analysis finished",
-      })
-    },
-    async getEntity() {
-      await this.getData();
-      this.entityRoot = []
-      this.entityRoot.push({group: 'nodes', data: {id: '-1', name: ''}})
-      let _this = this;
-      _this.getChildrenById(-1).then((res) => {
-        res.forEach(node => {
-          _this.entityRoot.push({
-            group: 'nodes',
-            data: {id: node["id"], name: node["name"], parent: '41576', classes: 'center-center'}
-          })
         })
+      },
+    async getEntity() {
+      if (this.type == 'dependency') await this.getData();
+      else await this.getArchModelData();
+      this.entityRoot = [];
+      let edgesParam = new Set();
+      const ref = this;
+      this.entityRoot.push({group: 'nodes', data: {id: -1, name: this.projectName}})
+      this.getChildrenById(-1).forEach((node) => {
+        const cat = ref.getEntityCategory(node);
+        this.entityRoot.push({
+          group: 'nodes',
+          data: {id: node["id"], name: node["name"], parent: -1, classes: 'center-center', category: cat}
+        });
+        edgesParam.add(node["id"]);
       })
-      console.log(_this.entityRoot);
+      ref.getRelationsByIds(edgesParam).forEach((edge) => {
+        const cat = ref.getRelationCategory(edge);
+        if (cat !== 'Contain' && cat !== 'Define') {
+          this.entityRoot.push({
+            group: 'edges',
+            data: {id: edge.id, source: String(edge["src"]), target: String(edge["dest"]), category: cat}
+          });
+        }
+      });
+      console.log("entityRoot: " + ref.entityRoot);
       cytoscape.use(fcose);
       let cy = cytoscape({
         container: document.getElementById('network'),
@@ -114,6 +202,7 @@ export default {
             node.css("width", size);
             node.css("height", size);
           });
+
           // this.layout({name: 'fcose', fit: true, nodeRepulsion: 99999,initialEnergyOnIncremental: 0.1,nestingFactor:0.1, animationEasing: 'ease-out'}).run();
         },
         layout: {
@@ -121,13 +210,14 @@ export default {
           fit: true,
           nodeRepulsion: 99999,
           animationDuration: 300,
-          spacingFactor: 1.8,
-          nodeDimensionsIncludeLabels: false,
+          spacingFactor: 1.2,
+          nodeDimensionsIncludeLabels: true,
+          avoidOverlap: true,
         },
-        // zoomingEnabled: false,
-        // userZoomingEnabled: false,
-        maxZoom: 1,
-        minZoom: 1,
+        zoomingEnabled: false,
+        userZoomingEnabled: false,
+        maxZoom: 2,
+        minZoom: 0.5,
         style: [
           {
             selector: 'node',
@@ -138,7 +228,16 @@ export default {
               'background-color': '#2B65EC'
             }
           },
-
+          {
+            selector: '[category = "Package"]',
+            style: {
+              'label': 'data(name)',
+              'font-size': '14px',
+              'background-opacity': 0.6,
+              'background-color': '#2B65EC',
+              "shape": "cut-rectangle",
+            }
+          },
           {
             selector: ':parent',
             style: {
@@ -170,31 +269,43 @@ export default {
             }
           }
         ],
-        elements: _this.entityRoot,
+        elements: this.entityRoot,
       });
       cy.on('tap', 'node', function (evt) {
         let target = evt.target;
-        console.log(target);
+        console.log("target: " + target);
+        console.dir(target);
         if (target.selected()) {
           target.children().forEach(ele => {
             cy.remove(ele)
           });
+          cy.remove(target);
+          cy.add(target)
         } else {
           let edgesParam = new Set();
-          _this.getChildrenById(Number(target.id())).then((res) => {
-            res.forEach(node => {
-              cy.add({
-                group: 'nodes',
-                data: {id: node["id"], name: node["name"], parent: target.id(), classes: 'center-center'}
-              })
-            });
+          ref.getChildrenById(Number(target.id())).forEach((node) => {
+            const cat = ref.getEntityCategory(node);
+            cy.add({
+              group: 'nodes',
+              data: {id: node["id"], name: node["name"], parent: target.id(), classes: 'center-center', category: cat}
+            })
           })
           cy.elements().forEach(ele => {
             edgesParam.add(Number(ele.data().id));
           });
-          _this.getRelationsByIds(edgesParam).then((res) => {
-            res.forEach(edge => {
-              const cat = _this.getRelationCategory(edge);
+          if (ref.type == 'target') {
+            ref.getRelationsByIdsTarget(edgesParam).forEach((edge) => {
+                const cat = edge.category;
+                if (cat !== 'Contain' && cat !== 'Define') {
+                  cy.add({
+                    group: 'edges',
+                    data: { id: edge.id, source: String(edge["src"]), target: String(edge["dest"]), category: edge.category }
+                  })
+                }
+              });
+          }else{
+            ref.getRelationsByIds(edgesParam).forEach((edge) => {
+              const cat = ref.getRelationCategory(edge);
               if (cat !== 'Contain' && cat !== 'Define') {
                 cy.add({
                   group: 'edges',
@@ -202,26 +313,27 @@ export default {
                 })
               }
             });
-          });
-          let layout = cy.layout({
+          }          
+        let layout = cy.layout({
             name: "fcose",
             fit: true,
             nodeRepulsion: 99999,
             randomize: true,
             animationDuration: 300,
             padding: 30,
-            nodeDimensionsIncludeLabels: false,
+            nodeDimensionsIncludeLabels: true,
             initialEnergyOnIncremental: 0.5,
             nestingFactor: 0.5,
-            spacingFactor: 2,
+            spacingFactor: 1.2,
+            avoidOverlap: true,
           })
           layout.run()
         }
       });
     },
-    async getConfig(){
+async getConfig() {
       vscode.postMessage({
-        command:'setConfig'
+        command: 'setConfig'
       })
     }
   }
@@ -239,7 +351,8 @@ export default {
 
 #network {
   width: 100%;
-  height: max(65vh, 800px);
+  //height: 100%;
+  height: max(80vh, 800px);
   overflow: auto;
   border: 1px solid #69f;
 }
